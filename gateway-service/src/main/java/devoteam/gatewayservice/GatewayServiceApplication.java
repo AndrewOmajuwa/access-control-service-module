2e18d38f1dd67eda7dc05a66b315f04e262606d6
Andrew Omajuwa <andrew.omajuwa@devoteam.com>
2022-06-01 15:49:59 +0200

    Modified application.yml(access-control-service) and GatewayServiceApplication

--- gateway-service/src/main/java/devoteam/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/devoteam/gatewayservice/GatewayServiceApplication.java
@@ -3,21 +3,10 @@ package devoteam.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayServiceApplication {
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/get")
-						.uri("http://localhost:8090/names"))
-				.build();
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
