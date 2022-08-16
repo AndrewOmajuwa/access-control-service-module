e8e8b2ad0cdb35061fd78e417be91b0f97792dfe
Andrew Omajuwa <andrew.omajuwa@devoteam.com>
2022-08-16 14:46:58 +0200

    fixed contex loads test

--- access-control-service/src/test/java/com/devoteam/accesscontrolservice/AccessControlServiceApplicationTests.java
+++ access-control-service/src/test/java/com/devoteam/accesscontrolservice/AccessControlServiceApplicationTests.java
@@ -1,10 +1,21 @@
 package com.devoteam.accesscontrolservice;
 
+import com.devoteam.accesscontrolservice.domain.KeycloakAdminClient;
+import com.devoteam.accesscontrolservice.domain.UserPostRequest;
+import com.devoteam.accesscontrolservice.util.Utility;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AccessControlServiceApplicationTests {
+
+	@MockBean
+	private KeycloakAdminClient keycloakAdminClient;
 
 	@Test
 	void contextLoads() {
