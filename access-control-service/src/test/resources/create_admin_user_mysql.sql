5b1f2ef0b51aa6e98e5338d1bf5d374a25c1fede
Andrew Omajuwa <andrew.omajuwa@devoteam.com>
2022-08-23 17:01:36 +0200

    Fixed pipeline issues

--- access-control-service/src/test/resources/create_admin_user_mysql.sql
+++ access-control-service/src/test/resources/create_admin_user_mysql.sql
@@ -62,6 +62,9 @@ values ('update');
 insert into profile (name)
 values ('test');
 
+insert into profile (name)
+values ('testDelete');
+
 insert into business_function_permission (business_function_id, permission_id)
 values (7, 2);
 
